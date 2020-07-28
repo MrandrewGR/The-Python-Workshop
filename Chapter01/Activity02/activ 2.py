ee7fa48228d7ae3b05b00f534d13561cc42ea7f1
Andrei <regkandrei@gmail.com>
2020-07-28 09:30:39 +0300

    начал уроки делать

---
+++ Chapter01/Activity02/activ 2.py
@@ -0,0 +1,4 @@
+#activity 2, find Pythagorean distance between x, y, and z
+x, y, z = 2, 3, 4
+Pythagorean_dist = (x ** 2 + y ** 2 + z ** 2) ** 0.5
+print(Pythagorean_dist)
---
+++ Chapter01/draft_1.py
@@ -0,0 +1,4 @@
+owner = 'Lawrence Ferlinghetti'
+age = 100
+print('The founder of City Lights Bookstore, {}, is now {} years old.'.format(owner, age))
+owner.
